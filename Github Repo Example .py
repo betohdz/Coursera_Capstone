847d5fc812cbfce5baf2755004321221903d4d11
betohdz <56895071+betohdz@users.noreply.github.com>
2019-12-16 14:47:51 -0600

    Add files via upload

---
+++ Github Repo Example .py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Welcome,This Notebook is be used to complete the IBM Data Science Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
